843b692b661266f98d071fcc41ca284edffbab86
Ayush Singh <r5radhasingh515@gmail.com>
2026-09-01 11:10:11 +0530

    Made changes in backtest

--- src/backtest.py
+++ src/backtest.py
@@ -17,11 +17,10 @@ def plot_equity_curve(df_bt, ticker="RELIANCE.NS"):
 
 
 def run_backtest(df_features, model, threshold=0.50):
-    
     df_bt = df_features.dropna(subset=["Target"]).copy()
     X = df_bt.drop(columns=["Target"])
-    
-    #to handle misisng Daily_retrun col
+
+    # to handle misisng Daily_retrun col
     if "Daily_Return" not in df_bt.columns:
         if "Close" in df_bt.columns:
             df_bt["Daily_Return"] = df_bt["Close"].pct_change()
@@ -31,29 +30,35 @@ def run_backtest(df_features, model, threshold=0.50):
             raise KeyError(
                 "Neither 'Daily_Return', 'Close', nor 'Log_Return' found in df_features."
             )
-            
+
     # Probabilistic Signal Generation
     probabilities = model.predict_proba(X)[:, 1]
-    df_bt['Signal'] = (probabilities >= threshold).astype(int)
-    
-    # Shift signal by 1 day to prevent lookahead bias 
-    df_bt['Strategy_Return'] = df_bt['Signal'].shift(1) * df_bt['Daily_Return']
+    df_bt["Signal"] = (probabilities >= threshold).astype(int)
+
+    # Shift signal by 1 day to prevent lookahead bias
+    df_bt["Strategy_Return"] = (
+        df_bt["Signal"].shift(1) * df_bt["Daily_Return"]
+    )
     df_bt.dropna(subset=["Strategy_Return"], inplace=True)
 
     # Cumulative growth calculation
-    df_bt['Benchmark_Cum'] = (1 + df_bt['Daily_Return']).cumprod()
-    df_bt['Strategy_Cum'] = (1 + df_bt['Strategy_Return']).cumprod()
+    df_bt["Benchmark_Cum"] = (1 + df_bt["Daily_Return"]).cumprod()
+    df_bt["Strategy_Cum"] = (1 + df_bt["Strategy_Return"]).cumprod()
 
     # Metric calculations
-    total_benchmark_return = df_bt['Benchmark_Cum'].iloc[-1] - 1
-    total_strategy_return = df_bt['Strategy_Cum'].iloc[-1] - 1
+    total_benchmark_return = df_bt["Benchmark_Cum"].iloc[-1] - 1
+    total_strategy_return = df_bt["Strategy_Cum"].iloc[-1] - 1
 
-    std_dev = df_bt['Strategy_Return'].std()
-    sharpe_ratio = (df_bt['Strategy_Return'].mean() / std_dev) * np.sqrt(252) if std_dev != 0 else 0.0
-    
+    std_dev = df_bt["Strategy_Return"].std()
+    sharpe_ratio = (
+        (df_bt["Strategy_Return"].mean() / std_dev) * np.sqrt(252)
+        if std_dev != 0
+        else 0.0
+    )
+
     # Maximum Drawdown calculation
-    rolling_max = df_bt['Strategy_Cum'].cummax()
-    drawdown = (df_bt['Strategy_Cum'] - rolling_max) / rolling_max
+    rolling_max = df_bt["Strategy_Cum"].cummax()
+    drawdown = (df_bt["Strategy_Cum"] - rolling_max) / rolling_max
     max_drawdown = drawdown.min()
 
     print("\n" + "=" * 50)
